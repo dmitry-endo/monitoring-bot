926a91c1ebc4662e2d40e493acbe6e0dc28c644f
dmitry-endo <creepylines@mail.ru>
2024-07-04 10:54:00 +0300

    the func for notification messages has been moved to specified file

--- handlers/event_monitoring.py
+++ handlers/event_monitoring.py
@@ -6,8 +6,9 @@ from aiogram.filters.chat_member_updated import \
     ChatMemberUpdatedFilter, JOIN_TRANSITION, LEAVE_TRANSITION, PROMOTED_TRANSITION, IS_ADMIN, MEMBER, RESTRICTED, IS_MEMBER
 from aiogram.types import ChatMemberUpdated, User, Chat
 
-### Load ID with specified chat for notifications
+# Load ID with the specified chat for notifications
 from config import NOTIF_CHAT_ID
+from .answer_template import answer_message
 
 router = Router()
 
@@ -15,33 +16,32 @@ utc_plus3 = timezone("Europe/Moscow")
 
 
 # Template func for bot notification messages
-def answer_message(event_user: User, event_chat: Chat, event_time: datetime):
-    date_format = "%d-%m-%Y"
-    time_format = "%X"
+# def answer_message(event_user: User, event_chat: Chat, event_time: datetime):
+#     date_format = "%d-%m-%Y"
+#     time_format = "%X"
 
-    # Show username with more common '@' at the start if user has one
-    if event_user.username is not None:
-        username = f'@{event_user.username}'
-    else:
-        username = event_user.username
-    
-    answer_msg = (
-        f'User full name: {event_user.full_name}\n'
-        f'Username: {username}\n'
-        f'User ID: {event_user.id}\n\n'
-        
-        f'{event_chat.type.capitalize()} title: {event_chat.title}\n'
-        f'{event_chat.type.capitalize()} ID: {event_chat.id}\n\n'
-        
-        f'Date: {event_time.strftime(date_format)}\n'
-        f'Time (UTC+3): {event_time.strftime(time_format)}'
-    )
-    
-    return answer_msg
+#     # Show username with more common '@' at the start if user has one
+#     if event_user.username is not None:
+#         username = f'@{event_user.username}'
+#     else:
+#         username = event_user.username
+
+#     answer_msg = (
+#         f'User full name: {event_user.full_name}\n'
+#         f'Username: {username}\n'
+#         f'User ID: {event_user.id}\n\n'
+
+#         f'{event_chat.type.capitalize()} title: {event_chat.title}\n'
+#         f'{event_chat.type.capitalize()} ID: {event_chat.id}\n\n'
+
+#         f'Date: {event_time.strftime(date_format)}\n'
+#         f'Time (UTC+3): {event_time.strftime(time_format)}'
+#     )
+
+#     return answer_msg
 
 
-### Handler for join as an Admin events
-### Or promotion to an Admin events
+# Handler for join or promotion as an Admin events
 @router.chat_member(
     ChatMemberUpdatedFilter(
         member_status_changed=PROMOTED_TRANSITION
@@ -56,18 +56,18 @@ async def event_is_admin(event: ChatMemberUpdated, bot: Bot):
     if event.old_chat_member.status == MEMBER or +RESTRICTED:
         await bot.send_message(
             chat_id=NOTIF_CHAT_ID,
-            text=(f"Someone's been promoted to an Admin in a {chat.type}!\n\n"
-                  f'{answer_message(user, chat, local_event_datetime)}')
+            text=(f"Someone has been promoted to an Admin in a {chat.type}!\n\n"
+                  f'{answer_message(user, chat, local_event_datetime, event)}')
         )
     else:
         await bot.send_message(
             chat_id=NOTIF_CHAT_ID,
             text=(f'Someone joined a {chat.type} as an Admin!\n\n'
-                  f'{answer_message(user, chat, local_event_datetime)}')
+                  f'{answer_message(user, chat, local_event_datetime, event)}')
         )
 
 
-### Handler for join as common member events
+# Handler for join as common member events
 @router.chat_member(
     ChatMemberUpdatedFilter(
         member_status_changed=JOIN_TRANSITION
@@ -81,11 +81,11 @@ async def event_is_member(event: ChatMemberUpdated, bot: Bot):
     await bot.send_message(
         chat_id=NOTIF_CHAT_ID,
         text=(f'Someone joined a {chat.type}!\n\n'
-              f'{answer_message(user, chat, local_event_datetime)}')
+              f'{answer_message(user, chat, local_event_datetime, event)}')
     )
 
 
-### Handler for demotion as an Admin events
+# Handler for demotion as an Admin events
 @router.chat_member(
     ChatMemberUpdatedFilter(
         member_status_changed=IS_ADMIN >> IS_MEMBER
@@ -98,12 +98,12 @@ async def event_is_not_admin(event: ChatMemberUpdated, bot: Bot):
 
     await bot.send_message(
         chat_id=NOTIF_CHAT_ID,
-        text=(f"Someone's been demoted as an Admin in a {chat.type}!\n\n"
-              f'{answer_message(user, chat, local_event_datetime)}')
+        text=(f"Someone has been demoted as an Admin in a {chat.type}!\n\n"
+              f'{answer_message(user, chat, local_event_datetime, event)}')
     )
 
 
-### Handler for leaving members events
+# Handler for leaving members events
 @router.chat_member(
     ChatMemberUpdatedFilter(
         member_status_changed=LEAVE_TRANSITION
@@ -117,5 +117,5 @@ async def event_is_not_member(event: ChatMemberUpdated, bot: Bot):
     await bot.send_message(
         chat_id=NOTIF_CHAT_ID,
         text=(f"Someone left from a {chat.type}!\n\n"
-              f'{answer_message(user, chat, local_event_datetime)}')
+              f'{answer_message(user, chat, local_event_datetime, event)}')
     )
