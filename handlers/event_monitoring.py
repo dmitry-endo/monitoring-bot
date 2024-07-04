d4056e7a5bcefaf579a7e96711d37b4fc6dbf739
dmitry-endo <creepylines@mail.ru>
2024-07-04 13:07:17 +0300

    little comments changes

--- handlers/event_monitoring.py
+++ handlers/event_monitoring.py
@@ -1,10 +1,9 @@
-from datetime import datetime
 from pytz import timezone
 
 from aiogram import Router, Bot
 from aiogram.filters.chat_member_updated import \
     ChatMemberUpdatedFilter, JOIN_TRANSITION, LEAVE_TRANSITION, PROMOTED_TRANSITION, IS_ADMIN, MEMBER, RESTRICTED, IS_MEMBER
-from aiogram.types import ChatMemberUpdated, User, Chat
+from aiogram.types import ChatMemberUpdated
 
 # Load ID with the specified chat for notifications
 from config import NOTIF_CHAT_ID
@@ -13,32 +12,6 @@ from .answer_template import answer_message
 router = Router()
 
 utc_plus3 = timezone("Europe/Moscow")
-
-
-# Template func for bot notification messages
-# def answer_message(event_user: User, event_chat: Chat, event_time: datetime):
-#     date_format = "%d-%m-%Y"
-#     time_format = "%X"
-
-#     # Show username with more common '@' at the start if user has one
-#     if event_user.username is not None:
-#         username = f'@{event_user.username}'
-#     else:
-#         username = event_user.username
-
-#     answer_msg = (
-#         f'User full name: {event_user.full_name}\n'
-#         f'Username: {username}\n'
-#         f'User ID: {event_user.id}\n\n'
-
-#         f'{event_chat.type.capitalize()} title: {event_chat.title}\n'
-#         f'{event_chat.type.capitalize()} ID: {event_chat.id}\n\n'
-
-#         f'Date: {event_time.strftime(date_format)}\n'
-#         f'Time (UTC+3): {event_time.strftime(time_format)}'
-#     )
-
-#     return answer_msg
 
 
 # Handler for join or promotion as an Admin events
@@ -56,7 +29,7 @@ async def event_is_admin(event: ChatMemberUpdated, bot: Bot):
     if event.old_chat_member.status == MEMBER or +RESTRICTED:
         await bot.send_message(
             chat_id=NOTIF_CHAT_ID,
-            text=(f"Someone has been promoted to an Admin in a {chat.type}!\n\n"
+            text=(f"Someone's been promoted to an Admin in a {chat.type}!\n\n"
                   f'{answer_message(user, chat, local_event_datetime, event)}')
         )
     else:
@@ -98,7 +71,7 @@ async def event_is_not_admin(event: ChatMemberUpdated, bot: Bot):
 
     await bot.send_message(
         chat_id=NOTIF_CHAT_ID,
-        text=(f"Someone has been demoted as an Admin in a {chat.type}!\n\n"
+        text=(f"Someone's been demoted as an Admin in a {chat.type}!\n\n"
               f'{answer_message(user, chat, local_event_datetime, event)}')
     )
 
